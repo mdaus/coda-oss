633a1493040b809c093d059dd8a53f96c4079687
jdsmith <J.Dan.Smith@mdaus.com>
2016-06-01 09:33:18 -0400

    turn off warning C4100 "unreferenced formal parameter"
    Turn off warning C4100 "unreferenced formal parameter" as it creates a
    lot of noise and really isn't very important (at all) to fix

--- build/build.py
+++ build/build.py
@@ -969,9 +969,10 @@ def configureCompilerOptions(self):
         # specifications properly.
         # Skipping warning 4512 about being unable to generate an assignment
         # operator (since we often do this intentionally).
+	# warning C4100: '...' : unreferenced formal parameter
         # For warnings, use /W4 because /Wall
         # gives us tons of warnings in the VS headers themselves
-        warningFlags = '/W4 /wd4290 /wd4512'
+        warningFlags = '/W4 /wd4290 /wd4512 /wd4100'
         if Options.options.warningsAsErrors:
             warningFlags += ' /WX'
 
