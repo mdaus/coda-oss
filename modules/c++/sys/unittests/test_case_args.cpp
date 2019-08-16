3ab546feb594ef0eea0cf3b7074348920d2c73ef
Vamsi Yadav <vamsiky@umich.edu>
2019-08-16 15:43:20 -0400

    Fixed style

--- modules/c++/sys/unittests/test_case_args.cpp
+++ modules/c++/sys/unittests/test_case_args.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 
-#include "TestCase.h"
+#include <TestCase.h>
 
 
 TEST_CASE_ARGS(readWords) {
-
-	std::string word1;
-	if(argc != 2) {
-		std::cerr << "Incorrect number of command line args\n";
-		throw except::Exception(Ctxt(
-			"Incorrect number of cmd line args"));
-	} else {
-		word1 = argv[1];
-		std::cout << "Parsed cmd line argument: " << word1 << std::endl;
-	}
+    if(argc != 2)
+    {
+        throw except::Exception(Ctxt(
+            "Incorrect number of cmd line args"));
+    }
+    else
+    {
+        std::string wordIn = argv[1];
+        std::cout << "Parsed cmd line argument: " << wordIn << std::endl;
+    }
 }
 
-int main(int argc, char** argv) {
-	try {
-		TEST_CHECK_ARGS(readWords);	
-	} catch (const except::Exception& ex) {
-		std::cerr << ex.toString() << std::endl;
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	} catch (...) {
-		std::cerr << "Unknown exception\n";
-	}
-	return 1;
-
+int main(int argc, char** argv)
+{
+    try
+    {
+        TEST_CHECK_ARGS(readWords);
+    }
+    catch (const except::Exception& ex)
+    {
+        std::cerr << ex.toString() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception\n";
+    }
+    return 1;
 }
