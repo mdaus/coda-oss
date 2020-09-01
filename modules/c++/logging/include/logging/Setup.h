63c4d1724e8684a41ad6d479c33e5f3735eeab64
Dan Smith <Dan.Smith@maxar.com>
2020-09-01 13:18:00 -0400

    make it easier to transition from std::auto_ptr to std::unique_ptr

--- modules/c++/logging/include/logging/Setup.h
+++ modules/c++/logging/include/logging/Setup.h
@@ -26,6 +26,8 @@
 #include <memory>
 #include <string>
 
+#include "mem/SharedPtrCpp11.h"
+
 #include "logging/Logger.h"
 
 namespace logging
@@ -45,7 +47,7 @@ namespace logging
  *  \param logCount - number of rotating logs to keep (default: 0 no rotation)
  *  \param logBytes - number of bytes per rotating log (default: 0 no rotation)
  */
-std::auto_ptr<logging::Logger> setupLogger(
+mem::auto_ptr<logging::Logger> setupLogger(
     const std::string& program, 
     const std::string& logLevel = "warning", 
     const std::string& logFile = "console",
