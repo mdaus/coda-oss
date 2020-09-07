cdb858a6f1573a06acdfc95c93ad04b5ad82e68d
Dan Smith <Dan.Smith@maxar.com>
2020-09-07 10:12:09 -0400

    Revert "switch NULL to nullptr"
    This reverts commit 44c2d30029b4f21d72ecf108d5743283afa6eb4b.

--- modules/c++/mt/include/mt/CPUAffinityInitializerWin32.h
+++ modules/c++/mt/include/mt/CPUAffinityInitializerWin32.h
@@ -41,7 +41,7 @@ class CPUAffinityInitializerWin32 : public AbstractCPUAffinityInitializer
 public:
     /*!
      * \todo Not yet implemented
-     * \returns nullptr
+     * \returns NULL
      */
     std::auto_ptr<CPUAffinityThreadInitializerWin32> newThreadInitializer()
     {
@@ -52,7 +52,7 @@ public:
 private:
     virtual CPUAffinityThreadInitializerWin32* newThreadInitializerImpl()
     {
-        return nullptr;
+        return NULL;
     }
 };
 }
