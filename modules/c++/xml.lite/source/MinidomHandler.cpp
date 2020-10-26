46abe1fb622992b2ea646b0cbce8a7a382937393
Dan Smith <dan.smith@maxar.com>
2020-10-26 17:13:50 -0400

    fix broken linux build

--- modules/c++/xml.lite/source/MinidomHandler.cpp
+++ modules/c++/xml.lite/source/MinidomHandler.cpp
@@ -90,9 +90,9 @@ void xml::lite::MinidomHandler::characters(const char *value, int length)
 bool xml::lite::MinidomHandler::characters(const wchar_t* const value_, const size_t length_)
 {
     #ifndef _WIN32
-    // As on Windows, this comes to us already encoded ... but UTF-16
-    const auto value = reinterpret_cast<const std::u16string::value_type*>(value_);
-    const std::u16string strValue(value, length_);
+    // As on Windows, this comes to us already encoded ... but UTF-32
+    const auto value = reinterpret_cast<const std::u32string::value_type*>(value_);
+    const std::u32string strValue(value, length_);
     std::string utf8Value;
     str::toUtf8(strValue, utf8Value);
 
