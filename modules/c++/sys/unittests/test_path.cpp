3c20f3edf88201927c689a968a28403b33c9c9c6
Dan Smith <dan.smith@maxar.com>
2021-03-15 13:31:28 -0400

    almost there unit-test for modifying env-vars

--- modules/c++/sys/unittests/test_path.cpp
+++ modules/c++/sys/unittests/test_path.cpp
@@ -218,6 +218,21 @@ TEST_CASE(testExpandEnvPathMultiple)
     TEST_ASSERT_EQ(expanded_paths.back(), expected_path);
 }
 
+
+TEST_CASE(testModifyVar)
+{
+    const sys::OS os;
+
+    const auto argv0 = sys::Path::expandEnvironmentVariables("${ARGV0@t}");
+    TEST_ASSERT_FALSE(argv0.empty());
+    
+    auto result = os.getSpecialEnv("0");  // i.e., ${0)
+    TEST_ASSERT_FALSE(result.empty());
+    const fs::path fsresult(result);
+    TEST_ASSERT_EQ(fsresult.stem(), "test_path");
+    // TEST_ASSERT_EQ(argv0, fsresult.filename());
+}
+
 }
 
 int main(int, char**)
@@ -228,6 +243,7 @@ int main(int, char**)
     TEST_CHECK(testExpandEnvTildePath);
     TEST_CHECK(testExpandEnvPathExists);
     TEST_CHECK(testExpandEnvPathMultiple);
+    TEST_CHECK(testModifyVar);
 
     return 0;
 }
