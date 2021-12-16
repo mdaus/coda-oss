549766d6c11187179f82402f995eea4e69b96550
Dan Smith <dan.smith@maxar.com>
2021-12-16 08:50:02 -0500

    Attributes::contains() no longer catches an exception

--- modules/c++/xml.lite/source/QName.cpp
+++ modules/c++/xml.lite/source/QName.cpp
@@ -89,28 +89,47 @@ const xml::lite::Uri& xml::lite::QName::getUri() const
     return mAssocUri;
 }
 
+static std::string flatten(const std::vector<std::string>& strs, size_t start = 0)
+{
+    std::string retval;
+    for (size_t i = start; i < strs.size(); i++)
+    {
+        retval += strs[i];
+    }
+    return retval;
+}
+
 xml::lite::Uri::Uri(const std::string& uri)
 {
-    // do some very simply sanity-checking on a URI
+    // Do some very simple sanity-checking on a URI; this could be (much?) more sophisticated.
     if (!uri.empty())
     {
         // https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
+        if (uri.length() <= 6) // "ab:CDEF"
+        {
+            // There's nothing that says we can't have short URIs, but does it
+            // make sense in actual use cases?
+            throw std::invalid_argument("string value '" + uri + "' is (too?) short.");
+        }
+
         const auto r = str::split(uri, ":");
         if (r.size() < 2)
         {
             throw std::invalid_argument("string value '" + uri + "' is not a URI.");
         }
 
-        if (r[0].size() <= 1)
+        if (r[0].length() <= 1)
         {
             // Is "a:" a real-world scheme?
             throw std::invalid_argument("string value '" + r[0] + "' is not a URI scheme.");
         }
 
-        if (r[1].size() < 5)
+        const auto path = flatten(r, 1); // don't care about other ':'s
+        if (path.length() <= 6)
         {
             // does it make sense to have a really short path?
-            throw std::invalid_argument("string value '" + r[0] + "' is too short for a URI path.");        
+            // in SIX we have "urn:us:gov"
+            throw std::invalid_argument("string value '" +  path + "' is (too?) short for a URI path.");        
         }
     }
     value = uri;
