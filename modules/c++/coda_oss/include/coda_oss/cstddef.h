91a729f5a2cadac711d59d3e490b4b531c133778
Dan Smith <dan.smith@maxar.com>
2022-02-04 12:38:40 -0500

    re-create from "master"

--- modules/c++/coda_oss/include/coda_oss/cstddef.h
+++ modules/c++/coda_oss/include/coda_oss/cstddef.h
@@ -1,10 +1,10 @@
 /* =========================================================================
- * This file is part of sys-c++
+ * This file is part of coda_oss-c++
  * =========================================================================
  *
  * (C) Copyright 2020, Maxar Technologies, Inc.
  *
- * sys-c++ is free software; you can redistribute it and/or modify
+ * coda_oss-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -23,19 +23,6 @@
 #pragma once
 
 #include <cstddef>
-
-// Need a fairly decent C++ compiler to use the real GSL.  This brings in more than 
-// we really need for span (e.g., gsl::narrow()), but it keeps things simple.
-#include "gsl/gsl.h"  // not gsl/byte; need #pragma here to turn off warnings
-
-namespace coda_oss
-{
-	#if defined(GSL_BYTE_H) // the above #include'd gsl/byte
-		using byte = gsl::byte;
-	#else // no gsl::byte, use our own
-        // https://en.cppreference.com/w/cpp/types/byte
-        enum class byte : unsigned char {};
-	#endif  // GSL_BYTE_H
-}
+#include "coda_oss/namespace_.h"  // coda_oss -> std
 
 #endif  // CODA_OSS_coda_oss_cstddef_h_INCLUDED_
