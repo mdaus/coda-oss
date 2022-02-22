8c0ce4d771c621e8d3e137f88f6bcbca40e51a2a
Dan Smith <dan.smith@maxar.com>
2022-02-22 09:48:52 -0500

    XERCES must use wchar_t

--- modules/c++/str/include/str/Encoding.h
+++ modules/c++/str/include/str/Encoding.h
@@ -95,6 +95,7 @@ inline coda_oss::u8string fromUtf8(std::string::const_pointer s)
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // We'll get strange errors, possibibly at link-time, if wchar_t is not a wchar_t type.
+// MSVC has an option to control this: https://docs.microsoft.com/en-us/cpp/build/reference/zc-wchar-t-wchar-t-is-native-type
 // https://en.cppreference.com/w/cpp/language/types
 // "It has the same size, signedness, and alignment as one of the integer types, but is a distinct type."
 static_assert(!std::is_same<wchar_t, uint16_t>::value, "wchar_t should not be the same as uint16_t");
