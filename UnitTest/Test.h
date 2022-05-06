97e4e63ed1bb6e465fe991c53cda8cc4bdf1c3d3
Dan Smith <dan.smith@maxar.com>
2022-05-06 09:04:21 -0400

    make functions consistent with macros

--- UnitTest/Test.h
+++ UnitTest/Test.h
@@ -16,11 +16,11 @@ inline std::wstring ToString(const uint16_t& q)
 
 #define TEST_ASSERT(X) Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue(X)
 
-template<typename T, typename U>
-inline void test_assert_eq_(T&& t, U&& u)
+template <typename TX1, typename TX2>
+inline void test_assert_eq_(const TX1& X1, const TX2& X2)
 {
-	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(t, u);
-	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(u, t);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(X1, X2);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(X2, X1);
 }
 #define TEST_ASSERT_EQ(X1, X2) test_assert_eq_(X1, X2);
 #define TEST_ASSERT_EQ_INT(X1, X2) TEST_ASSERT_EQ(X2, X1)
@@ -32,17 +32,17 @@ inline void test_assert_eq_(T&& t, U&& u)
 #define TEST_ASSERT_TRUE(X) Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue((X))
 #define TEST_ASSERT_FALSE(X) Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsFalse((X))
 
-template<typename T, typename U>
-inline void test_assert_not_eq_(T&& t, U&& u)
+template<typename TX1, typename TX2>
+inline void test_assert_not_eq_(const TX1& X1, const TX2& X2)
 {
-	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(t, u);
-	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(u, t);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(X1, X2);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreNotEqual(X2, X1);
 }
 #define TEST_ASSERT_NOT_EQ(X1, X2) test_assert_not_eq_(X1, X2);
-template<typename T, typename U>
-inline void test_assert_greater_(T&& t, U&& u)
+template <typename TX1, typename TX2>
+inline void test_assert_greater_(const TX1& X1, const TX2& X2)
 {
-	Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue(t > u);
+	Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue(X1 > X2);
 }
 #define TEST_ASSERT_GREATER(X1, X2) test_assert_greater_(X1, X2)
 
