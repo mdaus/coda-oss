150391f95409c91ba9c3c965bccd27dc0deefd3e
Dan Smith <dan.smith@maxar.com>
2022-05-06 10:44:43 -0400

    be sure to link in winsock

--- modules/c++/framework.h
+++ modules/c++/framework.h
@@ -2,6 +2,8 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
-#include <WinSock.h>
+#include <Winsock2.h>
+#pragma comment(lib, "Ws2_32")
+
 #undef min
 #undef max
