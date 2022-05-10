70a948521b2e6ea7a8de813e120d81d8d4654678
Dan Smith <dan.smith@maxar.com>
2022-05-10 13:34:47 -0400

    start adding "mem" unittests

---
+++ UnitTest/mem.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#define TEST_CASE(X) TEST_METHOD(X)
+
+namespace mem
+{
+
+//TEST_CLASS(test_scoped_cloneable_ptr){ public:
+//#include "mem/unittests/test_scoped_cloneable_ptr.cpp"
+//};
+
+}
