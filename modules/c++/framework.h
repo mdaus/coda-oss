eb5e3a9ae8bff691ce88469fdb111e12b7255ed9
Dan Smith <dan.smith@maxar.com>
2022-05-11 09:01:14 -0400

    set CODA_OSS_DLL from the command-line

--- modules/c++/framework.h
+++ modules/c++/framework.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <windows.h>
 #define NOMINMAX // no min()/max() macros
+#include <windows.h>
 #include <Winsock2.h>
 #pragma comment(lib, "Ws2_32")
-
