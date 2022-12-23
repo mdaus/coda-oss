b3b5ebde781e58c4dfd7d4478233fabbb984345a
J. Daniel Smith <dan@jdanielsmith.me>
2022-12-23 10:33:54 -0500

    use new exception utility routines

--- modules/c++/hdf5.lite/source/Info.cpp
+++ modules/c++/hdf5.lite/source/Info.cpp
@@ -22,54 +22,25 @@
 
 #include "hdf5/lite/Info.h"
 
-#include <stdexcept>
 #include <tuple> // std::ignore
 
-#include "except/Exception.h"
-#include "hdf5/lite/HDF5Exception.h"
+#include "H5.h"
+#include "hdf5.lite.h"
 
-// see https://docs.hdfgroup.org/archive/support/HDF5/doc1.8/cpplus_RM/readdata_8cpp-example.html
-#include <H5Cpp.h>
-
-hdf5::lite::FileInfo hdf5::lite::fileInfo(const coda_oss::filesystem::path& fileName)
+hdf5::lite::FileInfo fileInfo_(coda_oss::filesystem::path fileName)
 {
-    try
-    {
-        /*
-         * Turn off the auto-printing when failure occurs so that we can
-         * handle the errors appropriately
-         */
-        H5::Exception::dontPrint();
+    /*
+     * Open the specified file and the specified dataset in the file.
+     */
+    H5::H5File file(fileName.string(), H5F_ACC_RDONLY);
 
-        /*
-         * Open the specified file and the specified dataset in the file.
-         */
-        H5::H5File file(fileName.string(), H5F_ACC_RDONLY);
+    return hdf5::lite::FileInfo{};
+}
 
-        return hdf5::lite::FileInfo{};
-    }
-    // catch failure caused by the H5File operations
-    catch (const H5::FileIException& error)
-    {
-        const except::Context ctx(error.getDetailMsg(), __FILE__, __LINE__, error.getFuncName());
-        throw except::IOException(ctx);
-    }
-    // catch failure caused by the DataSet operations
-    catch (const H5::DataSetIException& error)
-    {
-        const except::Context ctx(error.getDetailMsg(), __FILE__, __LINE__, error.getFuncName());
-        throw DataSetException11(ctx);
-    }
-    // catch failure caused by the DataSpace operations
-    catch (const H5::DataSpaceIException& error)
-    {
-        const except::Context ctx(error.getDetailMsg(), __FILE__, __LINE__, error.getFuncName());
-        throw DataSpaceException11(ctx);
-    }
-    // catch failure caused by the DataType operations
-    catch (const H5::DataTypeIException& error)
-    {
-        const except::Context ctx(error.getDetailMsg(), __FILE__, __LINE__, error.getFuncName());
-        throw DataTypeException11(ctx);
-    }
+hdf5::lite::FileInfo hdf5::lite::fileInfo(coda_oss::filesystem::path fileName)
+{
+    hdf5::lite::FileInfo retval;
+    auto call_fileInfo_ = [&](void*) { retval = fileInfo_(fileName); };
+    details::try_catch_H5Exceptions(call_fileInfo_);
+    return retval;
 }
