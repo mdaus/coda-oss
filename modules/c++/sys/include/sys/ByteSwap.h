1231cd0045b6d3cb37614b7de03c4cdc07dc2665
Dan Smith <dan.smith@maxar.com>
2023-05-16 08:41:44 -0400

    skip std::span (for now)

--- modules/c++/sys/include/sys/ByteSwap.h
+++ modules/c++/sys/include/sys/ByteSwap.h
@@ -14,11 +14,18 @@ namespace sys
 template <typename T, typename U = T>
 struct ByteSwapCopyRunnable final : public sys::Runnable
 {
-    ByteSwapCopyRunnable() = delete;
-    ByteSwapCopyRunnable(coda_oss::span<const T> buffer, coda_oss::span<U> outputBuffer) :
-        mBuffer(buffer), mOutputBuffer(outputBuffer)
+    ByteSwapCopyRunnable(const T* buffer,
+                         size_t elemSize,
+                         size_t startElement,
+                         size_t numElements,
+                         U* outputBuffer) :
+        mBuffer(static_cast<const sys::byte*>(buffer) + startElement * elemSize),
+        mElemSize(static_cast<unsigned short>(elemSize)),
+        mNumElements(numElements),
+        mOutputBuffer(static_cast<sys::byte*>(outputBuffer) + startElement * elemSize)
     {
     }
+
     virtual ~ByteSwapCopyRunnable() = default;
     ByteSwapCopyRunnable(const ByteSwapCopyRunnable&) = delete;
     ByteSwapCopyRunnable& operator=(const ByteSwapCopyRunnable&) = delete;
@@ -27,44 +34,76 @@ struct ByteSwapCopyRunnable final : public sys::Runnable
 
     void run() override
     {
-        sys::byteSwap(mBuffer, mOutputBuffer);
+        sys::byteSwapV(mBuffer, mElemSize, mNumElements, mOutputBuffer);
     }
 
 private:
-    const coda_oss::span<const T> mBuffer;
-    const coda_oss::span<U> mOutputBuffer;
+    const sys::byte* const mBuffer;
+    const unsigned short mElemSize;
+    const size_t mNumElements;
+    sys::byte* const mOutputBuffer;
 };
+
+/*
+ * Threaded byte-swapping
+ *
+ * \param buffer Buffer to swap (contents will be overridden)
+ * \param elemSize Size of each element in 'buffer'
+ * \param numElements Number of elements in 'buffer'
+ * \param numThreads Number of threads to use for byte-swapping
+ */
+template <typename T, typename U = T>
+void threadedByteSwap(const T* buffer,
+                             size_t elemSize,
+                             size_t numElements,
+                             size_t numThreads,
+                             U* outputBuffer);
+template <typename T>
+inline void threadedByteSwap(T* buffer,
+              size_t elemSize,
+              size_t numElements,
+              size_t numThreads)
+{
+    threadedByteSwap(buffer, elemSize, numElements, numThreads, buffer);
+}
 
 /*
  * Threaded byte-swapping and copy
  *
  * \param buffer Buffer to swap
+ * \param elemSize Size of each element in 'buffer'
+ * \param numElements Number of elements in 'buffer'
  * \param numThreads Number of threads to use for byte-swapping
  * \param outputBuffer buffer to write into
  */
-template <typename T, typename U = T>
-inline void threadedByteSwap(coda_oss::span<const T> buffer,
-                     size_t numThreads,
-                     coda_oss::span<U> outputBuffer)
+template <typename T, typename U>
+inline void threadedByteSwap(const T* buffer,
+              size_t elemSize,
+              size_t numElements,
+              size_t numThreads,
+              U* outputBuffer)
 {
     if (numThreads <= 1)
     {
-        sys::byteSwap(buffer, outputBuffer);
+        sys::byteSwap(buffer, elemSize, numElements, outputBuffer);
         return;
     }
 
     mt::ThreadGroup threads;
-    const mt::ThreadPlanner planner(buffer.size(), numThreads);
+    const mt::ThreadPlanner planner(numElements, numThreads);
 
     size_t threadNum(0);
     size_t startElement(0);
     size_t numElementsThisThread(0);
     while (planner.getThreadInfo(threadNum++, startElement, numElementsThisThread))
     {
-        const coda_oss::span<const T> buffer_(buffer.data() + startElement, numElementsThisThread);
-        const coda_oss::span<T> outputBuffer_(outputBuffer.data() + startElement, numElementsThisThread);
+        auto thread = std::make_unique<ByteSwapCopyRunnable<T, U>>(
+                buffer,
+                elemSize,
+                startElement,
+                numElementsThisThread,
+                outputBuffer);
 
-        auto thread = std::make_unique<ByteSwapCopyRunnable<T, U>>(buffer_, outputBuffer_);
         threads.createThread(thread.release());
     }
     threads.joinAll();
