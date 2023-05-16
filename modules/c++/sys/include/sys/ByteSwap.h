f945cf9ac01dd3d69df6d46707ac091bf6307bd0
Dan Smith <dan.smith@maxar.com>
2023-05-16 09:40:21 -0400

    test other sys::byteSwap() combinations

--- modules/c++/sys/include/sys/ByteSwap.h
+++ modules/c++/sys/include/sys/ByteSwap.h
@@ -2,12 +2,7 @@
 #define CODA_OSS_sys_ByteSwap_h_INCLUDED_
 #pragma once
 
-#include <memory>
-
-#include <mt/ThreadPlanner.h>
-#include <mt/ThreadGroup.h>
-
-#include "Conf.h"
+#include "Runnable.h"
 
 namespace sys
 {
@@ -44,70 +39,6 @@ private:
     sys::byte* const mOutputBuffer;
 };
 
-/*
- * Threaded byte-swapping
- *
- * \param buffer Buffer to swap (contents will be overridden)
- * \param elemSize Size of each element in 'buffer'
- * \param numElements Number of elements in 'buffer'
- * \param numThreads Number of threads to use for byte-swapping
- */
-template <typename T, typename U = T>
-void threadedByteSwap(const T* buffer,
-                             size_t elemSize,
-                             size_t numElements,
-                             size_t numThreads,
-                             U* outputBuffer);
-template <typename T>
-inline void threadedByteSwap(T* buffer,
-              size_t elemSize,
-              size_t numElements,
-              size_t numThreads)
-{
-    threadedByteSwap(buffer, elemSize, numElements, numThreads, buffer);
-}
-
-/*
- * Threaded byte-swapping and copy
- *
- * \param buffer Buffer to swap
- * \param elemSize Size of each element in 'buffer'
- * \param numElements Number of elements in 'buffer'
- * \param numThreads Number of threads to use for byte-swapping
- * \param outputBuffer buffer to write into
- */
-template <typename T, typename U>
-inline void threadedByteSwap(const T* buffer,
-              size_t elemSize,
-              size_t numElements,
-              size_t numThreads,
-              U* outputBuffer)
-{
-    if (numThreads <= 1)
-    {
-        sys::byteSwap(buffer, elemSize, numElements, outputBuffer);
-        return;
-    }
-
-    mt::ThreadGroup threads;
-    const mt::ThreadPlanner planner(numElements, numThreads);
-
-    size_t threadNum(0);
-    size_t startElement(0);
-    size_t numElementsThisThread(0);
-    while (planner.getThreadInfo(threadNum++, startElement, numElementsThisThread))
-    {
-        auto thread = std::make_unique<ByteSwapCopyRunnable<T, U>>(
-                buffer,
-                elemSize,
-                startElement,
-                numElementsThisThread,
-                outputBuffer);
-
-        threads.createThread(thread.release());
-    }
-    threads.joinAll();
-}
 }
 
 #endif  // CODA_OSS_sys_ByteSwap_h_INCLUDED_
