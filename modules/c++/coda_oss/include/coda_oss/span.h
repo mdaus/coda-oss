67d3565b8cd2792394c7ed4d9267b2c0151d631a
Dan Smith <dan.smith@maxar.com>
2023-06-05 08:59:03 -0400

    get rid of castBytes() as it's hard to get it exactly right for general use

--- modules/c++/coda_oss/include/coda_oss/span.h
+++ modules/c++/coda_oss/include/coda_oss/span.h
@@ -19,9 +19,9 @@
  * see <http://www.gnu.org/licenses/>.
  *
  */
+#pragma once 
 #ifndef CODA_OSS_coda_oss_span_h_INCLUDED_
 #define CODA_OSS_coda_oss_span_h_INCLUDED_
-#pragma once
 
 #include <type_traits>
 
@@ -45,6 +45,9 @@ namespace coda_oss
 template <typename T>
 inline auto as_bytes(span<const T> s) noexcept
 {
+    // https://en.cppreference.com/w/cpp/types/is_trivially_copyable "... serialized to/from binary files ..."
+    static_assert(std::is_trivially_copyable<T>::value, "must be 'trivially' copyable.");
+
     const void* const p_ = s.data();
     auto const p = static_cast<const byte*>(p_);
     return span<const byte>(p, s.size_bytes());
@@ -58,6 +61,9 @@ inline auto as_bytes(span<T> s) noexcept
 template <typename T>
 inline span<byte> as_writable_bytes(span<T> s) noexcept
 {
+    // https://en.cppreference.com/w/cpp/types/is_trivially_copyable "... serialized to/from binary files ..."
+    static_assert(std::is_trivially_copyable<T>::value, "must be 'trivially' copyable.");
+
     static_assert(!std::is_const<T>::value, "T cannot be 'const'");
 
     void* const p_ = s.data();
