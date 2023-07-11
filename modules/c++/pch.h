eb93548ef9a0c31bdb0f50667ad625e5096cd078
Dan Smith <dan.smith@maxar.com>
2023-07-11 17:24:07 -0400

    Potential comparison of a constant with another constant.

--- modules/c++/pch.h
+++ modules/c++/pch.h
@@ -38,7 +38,6 @@ CODA_OSS_disable_warning_pop
 #pragma warning(disable: 4251) // '...': class '...' needs to have dll-interface to be used by clients of class '...'
 #pragma warning(disable: 4365) // '...': conversion from '...' to '...', signed/unsigned mismatch
 #pragma warning(disable: 5039) // '...': pointer or reference to potentially throwing function passed to '...' function under -EHc. Undefined behavior may occur if this function throws an exception.
-#pragma warning(disable: 6326) // Potential comparison of a constant with another constant.
 #pragma warning(disable: 6054) // String '...' might not be zero-terminated.
 #pragma warning(disable: 6387) // '...' could be '...'.
 #pragma warning(disable: 6305) // Potential mismatch between sizeof and countof quantities. Use sizeof() to scale byte sizes.
