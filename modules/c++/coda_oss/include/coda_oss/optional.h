1334a0827a9095a0583f8fd5cd6cd2761eb34a34
Dan Smith <Dan.Smith@maxar.com>
2023-08-02 13:09:32 -0400

    squash merge develop/remove-cpp11-workarounds

--- modules/c++/coda_oss/include/coda_oss/optional.h
+++ modules/c++/coda_oss/include/coda_oss/optional.h
@@ -23,8 +23,23 @@
 #define CODA_OSS_coda_oss_optional_h_INCLUDED_
 
 #include "coda_oss/CPlusPlus.h"
-#include "coda_oss/namespace_.h"
+
+// This logic needs to be here rather than <std/optional> so that `coda_oss::optional` will
+// be the same as `std::optional`.
+#ifndef CODA_OSS_HAVE_std_optional_
+    #define CODA_OSS_HAVE_std_optional_ 0  // assume no <optional>
+#endif
+#if CODA_OSS_cpp17 // C++17 for `__has_include()`
+    #if __has_include(<optional>)
+        #include <optional>
+        #undef CODA_OSS_HAVE_std_optional_
+        #define CODA_OSS_HAVE_std_optional_ 1  // provided by the implementation, probably C++17
+    #endif
+#endif // CODA_OSS_cpp17
+
+#if !CODA_OSS_HAVE_std_optional_
 #include "coda_oss/optional_.h"
+#endif
 
 // This logic needs to be here rather than <std/optional> so that `coda_oss::optional` will
 // be the same as `std::optional`.
