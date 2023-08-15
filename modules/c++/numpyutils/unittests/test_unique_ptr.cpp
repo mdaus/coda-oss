02f02bae6f770055a6b9c7a8916f563efb3fcb28
Dan Smith <Dan.Smith@maxar.com>
2023-08-15 12:53:42 -0400

    mem::AutoPtr is just for Python bindings

--- modules/c++/numpyutils/unittests/test_unique_ptr.cpp
+++ modules/c++/numpyutils/unittests/test_unique_ptr.cpp
@@ -22,7 +22,7 @@
 
 #include <std/memory>
 
-#include <mem/SharedPtr.h>
+#include <numpyutils/AutoPtr.h>
 
 #include "TestCase.h"
 
@@ -37,58 +37,45 @@ struct Foo final
     int mVal = 0;
 };
 
-TEST_CASE(testStdUniquePtr)
+static void f(const std::string& testName, mem::AutoPtr<Foo> p)
+{
+    TEST_ASSERT_NOT_NULL(p.get());
+    TEST_ASSERT_EQ(123, p->mVal);
+}
+TEST_CASE(memAutoPtr)
 {
     {
-        std::unique_ptr<Foo> fooCtor;
+        mem::AutoPtr<Foo> fooCtor;
         TEST_ASSERT_NULL(fooCtor.get());
 
         fooCtor.reset(new Foo(123));
-        TEST_ASSERT_NOT_EQ(nullptr, fooCtor.get());
+        TEST_ASSERT_NOT_NULL(fooCtor.get());
         TEST_ASSERT_EQ(123, fooCtor->mVal);
     }
     {
-        auto fooCtor = std::make_unique<Foo>(123);
-        TEST_ASSERT_NOT_EQ(nullptr, fooCtor.get());
+        mem::AutoPtr<Foo> fooCtor(new Foo(123));
+        TEST_ASSERT_NOT_NULL(fooCtor.get());
         TEST_ASSERT_EQ(123, fooCtor->mVal);
     }
     {
-        auto pFoos = std::make_unique<Foo[]>(123);  // 123 instances of Foo
-        TEST_ASSERT_NOT_EQ(nullptr, pFoos.get());
-        TEST_ASSERT_EQ(0, pFoos[0].mVal);
-        TEST_ASSERT_EQ(0, pFoos[122].mVal);
-    }
-}
-
-TEST_CASE(test_make_unique)
-{
-    {
-        auto fooCtor = std::make_unique<Foo>(123);
-        TEST_ASSERT_NOT_EQ(nullptr, fooCtor.get());
-        TEST_ASSERT_EQ(123, fooCtor->mVal);
+        mem::AutoPtr<Foo> fooCtor(new Foo(123));
+        mem::AutoPtr<Foo> other(fooCtor);
+        TEST_ASSERT_NOT_NULL(other.get());
+        TEST_ASSERT_EQ(123, other->mVal);
     }
     {
-        auto pFoos = std::make_unique<Foo[]>(123);  // 123 instances of Foo
-        TEST_ASSERT_NOT_EQ(nullptr, pFoos.get());
-        TEST_ASSERT_EQ(0, pFoos[0].mVal);
-        TEST_ASSERT_EQ(0, pFoos[122].mVal);
-    }
-
-    {
-        auto fooCtor = std::make_unique<Foo>(123);
-        TEST_ASSERT_NOT_EQ(nullptr, fooCtor.get());
-        TEST_ASSERT_EQ(123, fooCtor->mVal);
+        mem::AutoPtr<Foo> fooCtor(new Foo(123));
+        f(testName, fooCtor);
+        TEST_ASSERT_NULL(fooCtor.get());
     }
     {
-        auto pFoos = std::make_unique<Foo[]>(123);  // 123 instances of Foo
-        TEST_ASSERT_NOT_EQ(nullptr, pFoos.get());
-        TEST_ASSERT_EQ(0, pFoos[0].mVal);
-        TEST_ASSERT_EQ(0, pFoos[122].mVal);
+        std::unique_ptr<Foo> fooCtor(new Foo(123));
+        f(testName, std::move(fooCtor));
+        TEST_ASSERT_NULL(fooCtor.get());
     }
 }
 
 
 TEST_MAIN(
-   TEST_CHECK(testStdUniquePtr);
-   TEST_CHECK(test_make_unique);
+   TEST_CHECK(memAutoPtr);
    )
