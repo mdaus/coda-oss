806b0ff5518eca27d40c09c2fb8a0652eaca0fd4
Dan Smith <dan.smith@maxar.com>
2023-08-29 09:49:24 -0400

    FmtX() overloaded functions rather than FmtX() macro

--- modules/c++/str/include/str/Format.h
+++ modules/c++/str/include/str/Format.h
@@ -48,95 +48,96 @@ private:
     std::string mString;
 };
 
+}
+
 /*!
  *  \param format  The format
  *  \param ... Any printf like thing
  */
 //CODA_OSS_API std::string format(const char* format, ...);
 
-inline std::string format(const char* format)
+inline std::string FmtX(const char* format)
 {
-    return Format(format);
+    return str::Format(format);
 }
 
-inline std::string format(const char* format, const char* pStr)
+inline std::string FmtX(const char* format, const char* pStr)
 {
-    return Format(format, pStr);
+    return str::Format(format, pStr);
 }
-inline std::string format(const char* format, const std::string& s)
+inline std::string FmtX(const char* format, const std::string& s)
 {
-    return Format(format, s.c_str());
+    return str::Format(format, s.c_str());
 }
-inline std::string format(const char* format, int i)
+inline std::string FmtX(const char* format, int i)
 {
-    return Format(format, i);
+    return str::Format(format, i);
 }
-inline std::string format(const char* format, uint32_t i)
+inline std::string FmtX(const char* format, uint32_t i)
 {
-    return Format(format, i);
+    return str::Format(format, i);
 }
-inline std::string format(const char* format, ptrdiff_t l)
+inline std::string FmtX(const char* format, ptrdiff_t l)
 {
-    return Format(format, l);
+    return str::Format(format, l);
 }
-inline std::string format(const char* format, size_t ul)
+inline std::string FmtX(const char* format, size_t ul)
 {
-    return Format(format, ul);
+    return str::Format(format, ul);
 }
-inline std::string format(const char* format, float f)
+inline std::string FmtX(const char* format, float f)
 {
-    return Format(format, f);
+    return str::Format(format, f);
 }
 
-inline std::string format(const char* format, char ch, const char* pStr)
+inline std::string FmtX(const char* format, char ch, const char* pStr)
 {
-    return Format(format, ch, pStr);
+    return str::Format(format, ch, pStr);
 }
-inline std::string format(const char* format, char ch, const std::string& s)
+inline std::string FmtX(const char* format, char ch, const std::string& s)
 {
-    return Format(format, ch, s.c_str());
+    return str::Format(format, ch, s.c_str());
 }
-inline std::string format(const char* format, const std::string& s1, const std::string& s2)
+inline std::string FmtX(const char* format, const std::string& s1, const std::string& s2)
 {
-    return Format(format, s1.c_str(), s2.c_str());
+    return str::Format(format, s1.c_str(), s2.c_str());
 }
-inline std::string format(const char* format, const std::string& s, size_t ul)
+inline std::string FmtX(const char* format, const std::string& s, size_t ul)
 {
-    return Format(format, s.c_str(), ul);
+    return str::Format(format, s.c_str(), ul);
 }
-inline std::string format(const char* format, char ch1, char ch2)
+inline std::string FmtX(const char* format, char ch1, char ch2)
 {
-    return Format(format, ch1, ch2);
+    return str::Format(format, ch1, ch2);
 }
-inline std::string format(const char* format, size_t ul1, size_t ul2)
+inline std::string FmtX(const char* format, size_t ul1, size_t ul2)
 {
-    return Format(format, ul1, ul2);
+    return str::Format(format, ul1, ul2);
 }
-inline std::string format(const char* format, int i, const std::string& s)
+inline std::string FmtX(const char* format, int i, const std::string& s)
 {
-    return Format(format, i, s.c_str());
+    return str::Format(format, i, s.c_str());
 }
-inline std::string format(const char* format, int i, const char* pStr)
+inline std::string FmtX(const char* format, int i, const char* pStr)
 {
-    return Format(format, i, pStr);
+    return str::Format(format, i, pStr);
 }
 
-inline std::string format(const char* format, char ch1, char ch2, const std::string& s)
+inline std::string FmtX(const char* format, char ch1, char ch2, const std::string& s)
 {
-    return Format(format, ch1, ch2, s.c_str());
+    return str::Format(format, ch1, ch2, s.c_str());
 }
-inline std::string format(const char* format, int i1, int i2, unsigned long ul)
+inline std::string FmtX(const char* format, int i1, int i2, unsigned long ul)
 {
-    return Format(format, i1, i2, ul);
+    return str::Format(format, i1, i2, ul);
 }
-inline std::string format(const char* format, int i1, int i2, int i3)
+inline std::string FmtX(const char* format, int i1, int i2, int i3)
 {
-    return Format(format, i1, i2, i3);
+    return str::Format(format, i1, i2, i3);
 }
-inline std::string format(const char* format, const std::string& s1, const std::string& s2, uint32_t ui)
+inline std::string FmtX(const char* format, const std::string& s1, const std::string& s2, uint32_t ui)
 {
-    return Format(format, s1.c_str(), s2.c_str(), ui);
+    return str::Format(format, s1.c_str(), s2.c_str(), ui);
 }
 
-}
 #endif  // CODA_OSS_str_Format_h_INCLUDED_
