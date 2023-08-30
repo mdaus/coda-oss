6dacde2b5ba3705a65009dabe239fb7bd39034fe
Dan Smith <dan.smith@maxar.com>
2023-08-30 13:50:53 -0400

    new math.fast module

---
+++ modules/c++/math.fast/include/math/fast/Math.h
@@ -0,0 +1,67 @@
+/* =========================================================================
+ * This file is part of math.fast-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ * © Copyright 2023, Maxar Technologies, Inc.
+ *
+ * math.fast-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+#ifndef CODA_OSS_math_fast_Math_h_INCLUDED_
+#define CODA_OSS_math_fast_Math_h_INCLUDED_
+
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES // get M_PI, etc.
+#endif
+#include <math.h>
+#include <stdint.h>
+
+#include <complex>
+
+#include "coda_oss/CPlusPlus.h"
+#include "coda_oss/span.h"
+#include "config/Exports.h"
+
+#include "Span.h"
+
+namespace math
+{
+namespace fast
+{
+
+// "Overloads" for common routines in <math.h>
+
+CODA_OSS_API void Sin(span<const float>, span<float>);
+CODA_OSS_API void Sin(span<const double>, span<double>);
+
+CODA_OSS_API void Cos(span<const float>, span<float>);
+CODA_OSS_API void Cos(span<const double>, span<double>);
+
+CODA_OSS_API void SinCos(span<const float>, span<float> sines, span<float> cosines);
+CODA_OSS_API void SinCos(span<const double>, span<double> sines, span<double> cosines);
+
+CODA_OSS_API void Tan(span<const float>, span<float>);
+CODA_OSS_API void Tan(span<const double>, span<double>);
+
+CODA_OSS_API void ATan2(span<const float>, span<const float>, span<float>);
+CODA_OSS_API void ATan2(span<const double>, span<const double>, span<double>);
+
+}
+}
+
+#endif  // CODA_OSS_math_fast_Math_h_INCLUDED_
---
+++ modules/c++/math.fast/include/math/fast/Span.h
@@ -0,0 +1,40 @@
+/* =========================================================================
+ * This file is part of math.fast-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ * © Copyright 2023, Maxar Technologies, Inc.
+ *
+ * math.fast-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+#ifndef CODA_OSS_math_fast_Span_h_INCLUDED_
+#define CODA_OSS_math_fast_Span_h_INCLUDED_
+
+#include "coda_oss/span.h"
+
+// This lets us use just `span` inside of the *math::fast* namespace; see **Math.h**.
+namespace math
+{
+namespace fast
+{
+template <typename T>
+using span = coda_oss::span<T>;
+}
+}
+
+#endif  // CODA_OSS_math_fast_Span_h_INCLUDED_
