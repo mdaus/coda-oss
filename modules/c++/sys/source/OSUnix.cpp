df3928d7d65cbef431e406f39a2cbd03e48c6b7a
Matthew Hocking <MHocking@arka.org>
2026-02-23 12:02:32 -0500

    additional copyright info

--- modules/c++/sys/source/OSUnix.cpp
+++ modules/c++/sys/source/OSUnix.cpp
@@ -3,6 +3,7 @@
  * =========================================================================
  *
  * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ * (C) Copyright 2025-26 ARKA Group, L.P. All rights reserved
  *
  * sys-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
