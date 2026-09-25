c390427f98f608290e875894ff0b3a1420d82da9
Vaughn <63653004+holmesv3@users.noreply.github.com>
2026-09-25 15:11:36 -0400

    Convert ISO-8859-1 characters to UTF-8 (#805)
    * Convert ISO-8859-1 characters to UTF-8
    
    * mark as wide string
    
    * Try to convert chars
    
    * revert literal
    
    * another literal
    
    * maybe need the characters
    
    * try to determine which run is failing
    
    * use byte strings instead of unicode
    
    * oops
    
    * Fixed special E char
    
    * Surely this is it
    
    ---------
    
    Co-authored-by: Vaughn <vholmes@arka.org>

--- modules/c++/str/source/Manip.cpp
+++ modules/c++/str/source/Manip.cpp
@@ -341,32 +341,32 @@ inline char to_w1252_upper_(char ch)
     // See chart at: https://en.wikipedia.org/wiki/Windows-1252
     const auto u8 = static_cast<uint8_t>(ch);
 
-    constexpr uint8_t s_with_caron = 0x9a /* š */;
-    constexpr uint8_t oe = 0x9c /* œ */;
-    constexpr uint8_t z_with_caron = 0x9e /* ž */;
+    constexpr uint8_t s_with_caron = 0x9a /* Âš */;
+    constexpr uint8_t oe = 0x9c /* Âœ */;
+    constexpr uint8_t z_with_caron = 0x9e /* Âž */;
     if ((u8 == s_with_caron) || (u8 == oe) || (u8 == z_with_caron))
     {
         return ch ^ 0x10;    
     }
 
-    constexpr uint8_t a_with_grave = 0xe0 /* à */;
-    constexpr uint8_t o_with_diaeresis = 0xf6 /* ö */;
+    constexpr uint8_t a_with_grave = 0xe0 /* Ã  */;
+    constexpr uint8_t o_with_diaeresis = 0xf6 /* Ã¶ */;
     if ((u8 >= a_with_grave) && (u8 <= o_with_diaeresis))
     {
         return ch ^ 0x20;
     }
-    // U+00F7 ÷ DIVISION SIGN
-    constexpr uint8_t o_with_slash = 0xf8 /* ø */;
-    constexpr uint8_t small_thorn = 0xfe /* þ */;
+    // U+00F7 Ã· DIVISION SIGN
+    constexpr uint8_t o_with_slash = 0xf8 /* Ã¸ */;
+    constexpr uint8_t small_thorn = 0xfe /* Ã¾ */;
     if ((u8 >= o_with_slash) && (u8 <= small_thorn))
     {
         return ch ^ 0x20;
     }
 
-    constexpr uint8_t y_with_diaeresis = 0xff /* ÿ */;
+    constexpr uint8_t y_with_diaeresis = 0xff /* Ã¿ */;
     if (u8 == y_with_diaeresis)
     {
-        constexpr uint8_t Y_with_diaeresis = 0x9f /* Ÿ */;
+        constexpr uint8_t Y_with_diaeresis = 0x9f /* ÂŸ */;
         return Y_with_diaeresis;
     }
 
@@ -388,30 +388,30 @@ inline char to_w1252_lower_(char ch)
     // See chart at: https://en.wikipedia.org/wiki/Windows-1252
     const auto u8 = static_cast<uint8_t>(ch);
 
-    constexpr uint8_t S_with_caron = 0x8a /* Š */;
-    constexpr uint8_t OE = 0x8c /*Œ */;
-    constexpr uint8_t Z_with_caron = 0x8e /* Ž */;
+    constexpr uint8_t S_with_caron = 0x8a /* ÂŠ */;
+    constexpr uint8_t OE = 0x8c /*ÂŒ */;
+    constexpr uint8_t Z_with_caron = 0x8e /* ÂŽ */;
     if ((u8 == S_with_caron) || (u8 == OE) || (u8 == Z_with_caron))
     {
         return ch | 0x10;
     }
 
-    constexpr uint8_t Y_with_diaeresis = 0x9f /* Ÿ */;
+    constexpr uint8_t Y_with_diaeresis = 0x9f /* ÂŸ */;
     if (u8 == Y_with_diaeresis)
     {
-        constexpr uint8_t y_with_diaeresis = 0xff /* ÿ */;
+        constexpr uint8_t y_with_diaeresis = 0xff /* Ã¿ */;
         return y_with_diaeresis;
     }
 
-    constexpr uint8_t A_with_grave = 0xc0 /* À */;
-    constexpr uint8_t O_with_diaeresis = 0xd6 /* Ö */;
+    constexpr uint8_t A_with_grave = 0xc0 /* Ã€ */;
+    constexpr uint8_t O_with_diaeresis = 0xd6 /* Ã– */;
     if ((u8 >= A_with_grave) && (u8 <= O_with_diaeresis))
     {
         return ch | 0x20;
     }
-    // U+00D7 × MULTIPLICATION SIGN 
-    constexpr uint8_t O_with_slash = 0xd8 /* Ø */;
-    constexpr uint8_t capital_thorn = 0xde /* Þ */;
+    // U+00D7 Ã— MULTIPLICATION SIGN 
+    constexpr uint8_t O_with_slash = 0xd8 /* Ã˜ */;
+    constexpr uint8_t capital_thorn = 0xde /* Ãž */;
     if ((u8 >= O_with_slash) && (u8 <= capital_thorn))
     {
         return ch | 0x20;
--- modules/c++/str/unittests/test_base_convert.cpp
+++ modules/c++/str/unittests/test_base_convert.cpp
@@ -120,19 +120,19 @@ TEST_CASE(test_string_to_u8string_windows_1252)
 {
     // Windows-1252 only characters must be mapped to UTF-8
     {
-        const std::string input = "|\x80|";  // Windows-1252, "|Ä|"
+        const std::string input = "|\x80|";  // Windows-1252, "|euro sign|"
         const auto actual = str::to_u8string<str::W1252string>(input);
-        const std::u8string expected8{cast8('|'), cast8('\xE2'), cast8('\x82'), cast8('\xAC'), cast8('|')};  // UTF-8,  "|Ä|"
+        const std::u8string expected8{cast8('|'), cast8('\xE2'), cast8('\x82'), cast8('\xAC'), cast8('|')};  // UTF-8
         TEST_ASSERT_EQ(actual, expected8);
-        const std::u32string expected{U"|\u20AC|"};  // UTF-32,  "|Ä|"
+        const std::u32string expected{U"|\u20AC|"};  // UTF-32
         test_assert_eq(testName, actual, expected);
     }
     {
-        const std::string input = "|\x9F|";  // Windows-1252, "|ü|"
+        const std::string input = "|\x9F|";  // Windows-1252, "|Y with diaeresis|"
         const auto actual = str::to_u8string<str::W1252string>(input);
-        const std::u8string expected8{cast8('|'), cast8('\xC5'), cast8('\xB8'), cast8('|')};  // UTF-8,  "|ü|"
+        const std::u8string expected8{cast8('|'), cast8('\xC5'), cast8('\xB8'), cast8('|')};  // UTF-8
         TEST_ASSERT_EQ(actual, expected8);
-        const std::u32string expected{U"|\u0178|"};  // UTF-32,  "|ü|"
+        const std::u32string expected{U"|\u0178|"};  // UTF-32
         test_assert_eq(testName, actual, expected);
     }
     {
@@ -202,7 +202,7 @@ TEST_CASE(test_string_to_u8string_windows_1252)
 TEST_CASE(test_string_to_u8string_iso8859_1)
 {
     constexpr uint8_t nobreak_space = 0xa0;
-    constexpr uint8_t latin_small_letter_y_with_diaeresis = 0xff;  // 'ˇ'
+    constexpr uint8_t latin_small_letter_y_with_diaeresis = 0xff;  // '√ø'
     for (uint32_t ch = nobreak_space; ch <= latin_small_letter_y_with_diaeresis; ch++)  // ISO8859-1
     {
         const std::string input_ { '|', static_cast<std::string::value_type>(ch), '|'};
@@ -240,12 +240,12 @@ TEST_CASE(test_change_case)
     //const std::wstring abc_w = L"abc";
     //test_change_case_(testName, abc_w, ABC_w);
 
-    // Yes, this can really come up, "non classifiÈ" is French (Canadian) for "unclassified".
-    const std::string DEF_1252_{'D', '\xc9', 'F'}; // "D…F" Windows-1252
+    // Yes, this can really come up, "non classifi√©" is French (Canadian) for "unclassified".
+    const std::string DEF_1252_{'D', '\xc9', 'F'}; // "D√âF" Windows-1252
     const auto DEF_1252 = str::str<str::W1252string>(DEF_1252_);
     const auto DEF8 = str::to_u8string(DEF_1252);
 
-    const std::string def_1252_{'d', '\xe9', 'f'};  // "dÈf" Windows-1252
+    const std::string def_1252_{'d', '\xe9', 'f'};  // "d√©f" Windows-1252
     const auto def_1252 = str::str<str::W1252string>(def_1252_);
     const auto def8 = str::to_u8string(def_1252);
 
@@ -256,13 +256,13 @@ TEST_CASE(test_change_case)
 // https://en.wikipedia.org/wiki/%C3%89#Character_mappings
 static const coda_oss::u8string& classificationText_u8()
 {
-    static const auto retval(str::make_string<std::u8string>("A\xc3\x89IOU")); // UTF-8 "A…IOU"
+    static const auto retval(str::make_string<std::u8string>("A\xc3\x89IOU")); // UTF-8 "A√âIOU"
     return retval;
  }
 
 static const str::W1252string& classificationText_w1252()
  {
-    static const auto retval(str::make_string<str::W1252string>("A\xc9IOU"));  // ISO8859-1 "A…IOU"    
+    static const auto retval(str::make_string<str::W1252string>("A\xc9IOU"));  // ISO8859-1 "A√âIOU"    
     return retval;
  }
 
@@ -272,9 +272,9 @@ static const str::W1252string& classificationText_w1252()
  }
 
 // UTF-16 on Windows, UTF-32 on Linux
-static const wchar_t* classificationText_wide_() { return L"A\x00c9IOU"; } // "wide characters" "A…IOU"
-static std::u16string classificationText_u16() { return u"A\u00c9IOU"; } // UTF-16 "A…IOU"
-static std::u32string classificationText_u32() { return U"A\u00c9IOU"; } // UTF-32 "A…IOU"
+static const wchar_t* classificationText_wide_() { return L"A\x00c9IOU"; } // "wide characters" "A√âIOU"
+static std::u16string classificationText_u16() { return u"A\u00c9IOU"; } // UTF-16 "A√âIOU"
+static std::u32string classificationText_u32() { return U"A\u00c9IOU"; } // UTF-32 "A√âIOU"
 
 static std::string classificationText_platform() { return 
     sys::Platform == sys::PlatformType::Linux ? toString(classificationText_u8()) : str::testing::to_string(classificationText_w1252()); }
@@ -453,14 +453,14 @@ TEST_CASE(test_Windows1252_WIN32)
     // https://en.wikipedia.org/wiki/Windows-1252
     #if _WIN32
     // can convert with bit-twiddling
-    constexpr auto w1252_a1_ff = "°¢˛ˇ"; // <INVERTED EXCLAMATION MARK><CENT SIGN><LATIN SMALL LETTER THORN><LATIN SMALL LETTER Y WITH DIAERESIS>
-    //constexpr auto w1252_a1_ff = "\xa1\xa2\xfe\xff"; 
+    // "¬°¬¢√æ√ø"
+    constexpr auto w1252_a1_ff = "\xa1\xa2\xfe\xff"; 
     constexpr auto u16_w1252_a1_ff = u"\u00a1\u00a2\u00fe\u00ff";
     test_Windows1252_(testName, w1252_a1_ff, u16_w1252_a1_ff);
 
-    constexpr auto w1252 = "ÄÇÉÑÖÜáàâäãåéëíìîïñóòôöõúûü"; // these values must be mapped
-    //constexpr auto w1252 = "\x80\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8e" // these values must be mapped
-    //    "\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9e\x9f";
+    // "‚Ç¨‚Äö∆í‚Äû‚Ä¶‚Ä†‚Ä°ÀÜ‚Ä∞≈†‚Äπ≈í≈Ω‚Äò‚Äô‚Äú‚Äù‚Ä¢‚Äì‚ÄîÀú‚Ñ¢≈°‚Ä∫≈ì≈æ≈∏"
+    constexpr auto w1252 = "\x80\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8e" // these values must be mapped
+       "\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9e\x9f";
     constexpr auto u16_utf8 = u"\u20ac\u201a\u0192\u201e\u2026\u2020\u2021\u02c6\u2030\u0160\u2039\u0152\u017d"
         "\u2018\u2019\u201c\u201d\u2022\u2013\u2014\u02dc\u2122\u0161\u203a\u0153\u017e\u0178";
     test_Windows1252_(testName, w1252, u16_utf8);
